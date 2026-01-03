614fa421180185bc90d4b5c4837a3559c7d3f63c
snamdeo17 <shivendranamdev@gmail.com>
2026-01-03 19:33:41 +0530

    Add exception handling, Patient/Visit services, and Docker configuration

---
+++ src/main/java/com/bharatemr/exception/InvalidOtpException.java
@@ -0,0 +1,7 @@
+package com.bharatemr.exception;
+
+public class InvalidOtpException extends RuntimeException {
+    public InvalidOtpException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/bharatemr/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.bharatemr.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
